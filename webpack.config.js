5369a4fdcf735a276c2c37bff419808210a1d223
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-06-01 14:34:26 -0700

    Setup webpack/babel to transpile and compile a bundle.js file

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react'],
+          },
+        },
+      },
+    ],
+  },
+};
