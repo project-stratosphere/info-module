64bf9909bcb84e80a81edce8ec9aa1c66c5aacd5
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-06-04 16:41:21 -0700

    wrote regex for static files to limit 1-100, added basic get endpoints

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../client/dist/')));
+app.use(/(\/rooms\/100|\/rooms\/[1-9][0-9]|\/rooms\/[1-9])\b/, express.static(path.join(__dirname, '../client/dist/')));
+
+app.get('/api/rooms/:id/general', (req, res) => {
+  const { id } = req.params;
+
+  res.end(id);
+});
+
+app.get('/api/rooms/:id/amenities', (req, res) => {
+  const { id } = req.params;
+  res.end(id);
+});
 
 app.listen(3003);
