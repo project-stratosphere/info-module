3b6bf0d13d8e17b6a5edac41be57058d6319a59b
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-06-04 19:49:25 -0700

    changed pathname to fix bug

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,26 +5,27 @@ import Description from './Description.jsx';
 import Highlights from './Highlights.jsx';
 import Summary from './Summary.jsx';
 
-// const fakeGeneralInfo = JSON.parse('{"id":1,"title":"Practical Fantastic Home","location":"Rosenbaummouth","home_type":"Intelligent HOUSE","owner":{"name":"Pearl","avatar_url":"https://s3.amazonaws.com/uifaces/faces/twitter/falvarad/128.jpg"},"property_features":{"guests":3,"bedrooms":0,"beds":5,"baths":4},"highlights":[{"head":"Indoor fireplace","body":""},{"head":"Self check-in","body":""},{"head":"Great check-in experience","body":""}],"short_description":"Harum perspiciatis minima tempore nobis sed.","more_description":[{"head":"The space","body":"Adipisci reprehenderit repudiandae blanditiis. Dolor assumenda consectetur fuga. Eveniet amet optio quo veniam vel at quas. Nisi et autem et magni dolorem accusamus ipsam eius laborum."},{"head":"Guest access","body":"Labore rerum sed tempore ipsa magnam odio ducimus modi error."}]}');
-// const fakeAmenities = JSON.parse('{"id":1,"items":[{"category_head":"Industrial","category_items":[{"amenity_description":"primary","supplemental_description":"Qui ut qui reiciendis omnis id delectus assumenda."},{"amenity_description":"Pa\'anga","supplemental_description":"Optio odio odio."},{"amenity_description":"Customer-focused","supplemental_description":"Vel sed ad optio et eligendi ab."}]}]}');
+const fakeGeneralInfo = JSON.parse('{"id":1,"title":"Practical Fantastic Home","location":"Rosenbaummouth","home_type":"Intelligent HOUSE","owner":{"name":"Pearl","avatar_url":"https://s3.amazonaws.com/uifaces/faces/twitter/falvarad/128.jpg"},"property_features":{"guests":3,"bedrooms":0,"beds":5,"baths":4},"highlights":[{"head":"Indoor fireplace","body":""},{"head":"Self check-in","body":""},{"head":"Great check-in experience","body":""}],"short_description":"Harum perspiciatis minima tempore nobis sed.","more_description":[{"head":"The space","body":"Adipisci reprehenderit repudiandae blanditiis. Dolor assumenda consectetur fuga. Eveniet amet optio quo veniam vel at quas. Nisi et autem et magni dolorem accusamus ipsam eius laborum."},{"head":"Guest access","body":"Labore rerum sed tempore ipsa magnam odio ducimus modi error."}]}');
+const fakeAmenities = JSON.parse('{"id":1,"items":[{"category_head":"Industrial","category_items":[{"amenity_description":"primary","supplemental_description":"Qui ut qui reiciendis omnis id delectus assumenda."},{"amenity_description":"Pa\'anga","supplemental_description":"Optio odio odio."},{"amenity_description":"Customer-focused","supplemental_description":"Vel sed ad optio et eligendi ab."}]}]}');
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.id = window.location.pathname.match(/[0-9]+/)[0] || 1;
     this.state = {
-      generalInfo: {},
-      amenities: {},
+      generalInfo: fakeGeneralInfo,
+      amenities: fakeAmenities,
     };
   }
 
   componentDidMount() {
-    console.log('hello');
+    this.getGeneralInfo();
+    this.getAmenities();
   }
 
   getGeneralInfo() {
     $.ajax({
-      url: `/api/${this.id}/general`,
+      url: `/api/room/${this.id}/general`,
       type: 'GET',
       success: data => this.setState({
         generalInfo: JSON.parse(data),
@@ -34,7 +35,7 @@ export default class App extends React.Component {
 
   getAmenities() {
     $.ajax({
-      url: `/api/${this.id}/amenities`,
+      url: `/api/room/${this.id}/amenities`,
       type: 'GET',
       success: data => this.setState({
         amenities: JSON.parse(data),
--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(/(\/rooms\/100|\/rooms\/[1-9][0-9]|\/rooms\/[1-9])\b/, express.static(path.join(__dirname, '../client/dist/')));
 
-app.get('/api/rooms/:id/general', (req, res) => {
+app.get('/api/room/:id/general', (req, res) => {
   const { id } = req.params;
   db.getGeneralInfo(id)
     .then((data) => {
@@ -19,7 +19,7 @@ app.get('/api/rooms/:id/general', (req, res) => {
     });
 });
 
-app.get('/api/rooms/:id/amenities', (req, res) => {
+app.get('/api/room/:id/amenities', (req, res) => {
   const { id } = req.params;
   db.getAmenities(id)
     .then((data) => {
