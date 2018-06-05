27c75b8fcdc4de75055aabb00f9ffdf63c973f4d
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-06-04 17:18:11 -0700

    server responds with data or err based on DB response to valid paths

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const db = require('../database');
 
 const app = express();
 
@@ -7,13 +8,28 @@ app.use(/(\/rooms\/100|\/rooms\/[1-9][0-9]|\/rooms\/[1-9])\b/, express.static(pa
 
 app.get('/api/rooms/:id/general', (req, res) => {
   const { id } = req.params;
-
-  res.end(id);
+  db.getGeneralInfo(id)
+    .then((data) => {
+      res.statusCode = 200;
+      res.type = 'json';
+      res.end(JSON.stringify(data[0]));
+    })
+    .catch((err) => {
+      res.end(JSON.stringify(err));
+    });
 });
 
 app.get('/api/rooms/:id/amenities', (req, res) => {
   const { id } = req.params;
-  res.end(id);
+  db.getAmenities(id)
+    .then((data) => {
+      res.end(JSON.stringify(data[0]));
+    })
+    .catch((err) => {
+      res.statusCode = 200;
+      res.type = 'json';
+      res.end(JSON.stringify(err));
+    });
 });
 
 app.listen(3003);
