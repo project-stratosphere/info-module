6f8c98e4309c64dbef1a7522921a239ed6fdc6f9
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-06-06 19:37:47 -0700

    seperated App styled components into separate file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import $ from 'jquery';
-import styled from 'styled-components';
 import Amenities from './Amenities.jsx';
 import Description from './Description.jsx';
 import Highlights from './Highlights.jsx';
 import Summary from './Summary.jsx';
+import { Wrapper } from './styles/App.styles';
 
 const fakeGeneralInfo = JSON.parse('{"id":1,"title":"Practical Fantastic Home","location":"Rosenbaummouth","home_type":"Intelligent HOUSE","owner":{"name":"Pearl","avatar_url":"https://s3.amazonaws.com/uifaces/faces/twitter/falvarad/128.jpg"},"property_features":{"guests":3,"bedrooms":0,"beds":5,"baths":4},"highlights":[{"head":"Indoor fireplace","body":""},{"head":"Self check-in","body":""},{"head":"Great check-in experience","body":""}],"short_description":"Harum perspiciatis minima tempore nobis sed.","more_description":[{"head":"The space","body":"Adipisci reprehenderit repudiandae blanditiis. Dolor assumenda consectetur fuga. Eveniet amet optio quo veniam vel at quas. Nisi et autem et magni dolorem accusamus ipsam eius laborum."},{"head":"Guest access","body":"Labore rerum sed tempore ipsa magnam odio ducimus modi error."}]}');
 const fakeAmenities = JSON.parse('{"id":1,"items":[{"category_head":"Industrial","category_items":[{"amenity_description":"primary","supplemental_description":"Qui ut qui reiciendis omnis id delectus assumenda."},{"amenity_description":"Pa\'anga","supplemental_description":"Optio odio odio."},{"amenity_description":"Customer-focused","supplemental_description":"Vel sed ad optio et eligendi ab."}]}]}');
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
 
 export default class App extends React.Component {
   constructor(props) {
