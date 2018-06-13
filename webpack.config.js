ba5197c7f372eacf03c5d47fb9a7f4100f66265f
ivan-cordoba <ivan.cordoba95@gmail.com>
2018-06-13 11:14:08 -0700

    changed bundle name to infobundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: path.join(__dirname, '/client/src/index.jsx'),
   output: {
-    filename: 'bundle.js',
+    filename: 'infobundle.js',
     path: path.join(__dirname, '/client/dist'),
   },
   mode: 'production',
